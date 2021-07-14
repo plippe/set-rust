f535da248eb9edf811ecb43747044ac4d9b25c40
Philippe Vinchon <p.vinchon@gmail.com>
2021-07-14 21:21:03 +0100

    Add tests to catch non swapped cards

--- src/cards/hand.rs
+++ src/cards/hand.rs
@@ -23,7 +23,7 @@ impl Hand {
         let mut stock = stock.clone();
         let mut cards = Vec::new();
 
-        while !Self::is_valid(&cards) && Self::is_swap(&cards, set) {
+        while !(Self::is_valid(&cards) && Self::is_swap(&cards, set)) {
             cards = self
                 .cards
                 .clone()
@@ -56,5 +56,102 @@ impl Hand {
 
     fn is_swap(cards: &[Card], set: &Set) -> bool {
         !cards.iter().any(|&c| set.contains(c))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[quickcheck_macros::quickcheck]
+    fn from_stock_length(seed: u64) {
+        let stock = Stock::from_seed(seed);
+        let (_, hand) = Hand::from_stock(&stock);
+
+        assert!(hand.cards.len() == 12)
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn from_stock_unique(seed: u64) {
+        let stock = Stock::from_seed(seed);
+        let (_, hand) = Hand::from_stock(&stock);
+
+        assert!(hand.cards.iter().unique().collect_vec().len() == 12)
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn from_stock_contains_set(seed: u64) {
+        let stock = Stock::from_seed(seed);
+        let (_, hand) = Hand::from_stock(&stock);
+
+        assert!(hand
+            .cards
+            .iter()
+            .tuple_combinations()
+            .any(|(a, b, c)| Set::try_from_cards(*a, *b, *c).is_ok()))
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn swap_length(seed: u64) {
+        let stock = Stock::from_seed(seed);
+        let (stock, hand) = Hand::from_stock(&stock);
+        let set = hand
+            .cards
+            .iter()
+            .tuple_combinations()
+            .find_map(|(a, b, c)| Set::try_from_cards(*a, *b, *c).ok())
+            .unwrap();
+        let (_, hand) = hand.swap(&stock, &set);
+
+        assert!(hand.cards.len() == 12)
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn swap_unique(seed: u64) {
+        let stock = Stock::from_seed(seed);
+        let (stock, hand) = Hand::from_stock(&stock);
+        let set = hand
+            .cards
+            .iter()
+            .tuple_combinations()
+            .find_map(|(a, b, c)| Set::try_from_cards(*a, *b, *c).ok())
+            .unwrap();
+        let (_, hand) = hand.swap(&stock, &set);
+
+        assert!(hand.cards.iter().unique().collect_vec().len() == 12)
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn swap_contains_set(seed: u64) {
+        let stock = Stock::from_seed(seed);
+        let (stock, hand) = Hand::from_stock(&stock);
+        let set = hand
+            .cards
+            .iter()
+            .tuple_combinations()
+            .find_map(|(a, b, c)| Set::try_from_cards(*a, *b, *c).ok())
+            .unwrap();
+        let (_, hand) = hand.swap(&stock, &set);
+
+        assert!(hand
+            .cards
+            .iter()
+            .tuple_combinations()
+            .any(|(a, b, c)| Set::try_from_cards(*a, *b, *c).is_ok()))
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn swap_contains_new_cards(seed: u64) {
+        let stock = Stock::from_seed(seed);
+        let (stock, hand) = Hand::from_stock(&stock);
+        let set = hand
+            .cards
+            .iter()
+            .tuple_combinations()
+            .find_map(|(a, b, c)| Set::try_from_cards(*a, *b, *c).ok())
+            .unwrap();
+        let (_, hand) = hand.swap(&stock, &set);
+
+        assert!(!hand.cards.iter().any(|card| set.contains(*card)))
     }
 }
